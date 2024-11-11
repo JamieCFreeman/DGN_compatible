c48fd083c0cd5417bf455a43ac89b0702713ccae
JamieCFreeman <jcf236@cornell.edu>
2024-11-11 16:03:02 -0600

    working on parsing qc files to create qc output from pipeline

---
+++ scripts/DGN_pipeline_qc/dup_qc.py
@@ -0,0 +1,56 @@
+
+# 2024-11-11 JCF
+
+# Purpose: 
+# To pull duplication metrics from a metrics file written by 
+#  GATK MarkDuplicates
+
+# Input:
+# metrics file as written by GATK markduplicates
+
+
+###############################################################
+# Helper functions
+def lines_in(f):
+	f        = open(f,'r')
+	lines    = f.readlines()
+	f.close()
+	return lines
+
+def dup_list(lines):
+	'''
+	Take raw lines of duplication metrics file and parse to DuplicationMetrics object
+	'''
+	# Line 7 is the data we want
+	i = lines[7].strip().split('\t')
+	i[0] = '"' + i[0] + '"'
+	return i
+
+###############################################################
+# Class definition
+class DuplicationMetrics:
+  def __init__(metrics, filename):
+    metrics.filename = filename
+    metrics.raw = lines_in(filename)
+    metrics.list = dup_list(metrics.raw)
+    metrics.library                        = metrics.list[0]
+    metrics.unpaired_reads_examined        = int( metrics.list[1] )
+    metrics.read_pairs_examined            = int( metrics.list[2] )
+    metrics.secondary_or_supplementary_rds = int( metrics.list[3] )
+    metrics.unmapped_reads                 = int( metrics.list[4] )
+    metrics.unpaired_read_duplicates       = int( metrics.list[5] )
+    metrics.read_pair_duplicates           = int( metrics.list[6] )
+    metrics.read_pair_optical_duplicates   = int( metrics.list[7] )
+    metrics.percent_duplication            = float( metrics.list[8] )
+    metrics.estimated_library_size         = int( metrics.list[9] )
+
+###############################################################
+
+if __name__ == "__main__":
+	import sys
+	dup_file = sys.argv[1]
+	p1       = DuplicationMetrics(dup_file)
+	print( p1.library + "\t" + str(p1.percent_duplication) )
+
+
+
---
+++ scripts/DGN_pipeline_qc/parse_flagstat.py
@@ -0,0 +1,77 @@
+# 2024-11-11 JCF
+
+# Purpose: 
+# To pull duplication metrics from a metrics file written by 
+#  GATK MarkDuplicates
+
+# Input:
+# metrics file as written by GATK markduplicates
+
+###############################################################
+# # Example flasgstat output file
+# 31194858 + 0 in total (QC-passed reads + QC-failed reads)
+# 31194858 + 0 primary
+# 0 + 0 secondary
+# 0 + 0 supplementary
+# 0 + 0 duplicates
+# 0 + 0 primary duplicates
+# 30879493 + 0 mapped (98.99% : N/A)
+# 30879493 + 0 primary mapped (98.99% : N/A)
+# 31194858 + 0 paired in sequencing
+# 15597429 + 0 read1
+# 15597429 + 0 read2
+# 25196938 + 0 properly paired (80.77% : N/A)
+# 30780142 + 0 with itself and mate mapped
+# 99351 + 0 singletons (0.32% : N/A)
+# 1817110 + 0 with mate mapped to a different chr
+# 259848 + 0 with mate mapped to a different chr (mapQ>=5)
+
+###############################################################
+# Helper functions
+def lines_in(f):
+	f        = open(f,'r')
+	lines    = f.readlines()
+	f.close()
+	return lines
+
+def transform_lines(lines):
+	l = [ x.split(' ')[0] for x in lines ] 
+	return l
+
+###############################################################
+# Class definition
+class Flagstat:
+  def __init__(metrics, filename):
+    metrics.filename              = filename
+    metrics.raw                   = lines_in(filename)
+    metrics.list                  = transform_lines(metrics.raw)
+    metrics.library               = filename.split('.stats')[0]
+    metrics.total_reads           = int( metrics.list[0] )
+    metrics.primary_align         = int( metrics.list[1] )
+    metrics.secondary_align       = int( metrics.list[2] )
+    metrics.suppl_align           = int( metrics.list[3] )
+    metrics.duplicates            = int( metrics.list[4] )
+    metrics.primary_duplicates    = int( metrics.list[5] )
+    metrics.mapped                = int( metrics.list[6] )
+    metrics.primary_mapped        = int( metrics.list[7] )
+    metrics.paired_reads          = int( metrics.list[8] )
+    metrics.read1                 = int( metrics.list[9] )
+    metrics.read2                 = int( metrics.list[10] )
+    metrics.properly_paired       = int( metrics.list[11] )
+    metrics.both_reads_mapped     = int( metrics.list[12] )
+    metrics.singletons            = int( metrics.list[13] )
+    metrics.mate_map_diff_chr     = int( metrics.list[14] )
+    metrics.mate_map_diff_chr_MQ  = int( metrics.list[15] )
+
+###############################################################
+if __name__ == "__main__":
+	import sys
+	stats_file = sys.argv[1]
+	p1 = Flagstat(stats_file)
+	s1 ='total_reads' + '\t' + 'perc_mapped_reads' + '\t' + 'properly_paired_reads'
+	print( s1 )
+	s2 = str(p1.total_reads) + '\t' \
+	+ str(round(p1.mapped / p1.total_reads, 4)) + '\t' \
+	+ str(round(p1.properly_paired / p1.total_reads, 4))
+	print( s2)
+
